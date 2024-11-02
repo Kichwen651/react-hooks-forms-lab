7aaf5359b33fa532aa3bdfdcf11dc5e67c0b0e1b
Kichwen651 <victor.kichwen@student.moringaschool.com>
2024-11-02 15:36:13 +0300

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,27 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ selectedCategory, onCategoryChange, search, onSearchChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
+      <label htmlFor="category">Filter by category:</label>
+      <select id="category" value={selectedCategory} onChange={onCategoryChange}>
+        <option value="All">All</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
       </select>
+
+      <label htmlFor="search">Search:</label>
+      <input
+        id="search"
+        type="text"
+        value={search} // Use the 'search' prop here
+        onChange={onSearchChange}
+        placeholder="Search items..."
+      />
     </div>
   );
 }
 
 export default Filter;
+
